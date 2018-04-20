2ec7c7f47294f5a7e6e860f107be036984acb64f
Mason Staugler <mason@staugler.net>
2018-04-20 02:13:20 -0400

    4:06 Raw mode, quit.
    I'm using a crate called Termion. I suspect that I'll prefer the Termios crate instead since it's lower level. On the other hand, this was fairly easy.
    
    It does a lot of that thing where you "use" something that adds behavior to the structs in other packages. For example, adding `into_raw_mode` on stdout. I think this is the Traits but, honestly, I still don't understand why people like this. Why can't you `termion::into_raw_mode(stdout)` instead of augmenting the stdout struct behind the scenes?

--- med/src/main.rs
+++ med/src/main.rs
@@ -1,8 +1,27 @@
+extern crate termion;
+
+use std::{thread, time};
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{
+    self,
+    BufRead,
+    BufReader,
+    Read,
+    stdin,
+    stdout,
+    Stdout,
+    Write,
+};
+use std::process::exit;
+
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::screen::{AlternateScreen, ToAlternateScreen, ToMainScreen};
 
 struct Editor {
-    pub lines: Vec<String>,
+    lines: Vec<String>,
+    screen: AlternateScreen<RawTerminal<Stdout>>,
 }
 
 impl Editor {
@@ -14,11 +33,63 @@ impl Editor {
                 },
                 _ => panic!("failed to open file"),
             },
+            screen: AlternateScreen::from(stdout().into_raw_mode().unwrap()),
+        }
+    }
+
+    fn run(&mut self) {
+        write!(self.screen, "{}", ToAlternateScreen).unwrap();
+
+        loop {
+            self.render();
+            match self.handle_input() {
+                true => break,
+                _ => {},
+            }
+        }
+
+        write!(self.screen, "{}", ToMainScreen).unwrap();
+    }
+
+    fn render(&self) {
+    }
+
+    fn handle_input(&mut self) -> bool {
+        //let line = {
+        //    let mut b = String::new();
+        //    match io::stdin().read_line(&mut b) {
+        //        Ok(_) => b,
+        //        _ => panic!("TODO failed getting user input. Is that scary?"),
+        //    }
+        //};
+
+        //match line.as_ref() {
+        //    "q\n" => exit(0),
+        //    any => println!("{:?} isn't a command. Hit q to quit.", any),
+        //}
+        match stdin().keys().next().unwrap() {
+            Ok(Key::Ctrl('q')) => {
+                write!(self.screen, "Quitting...\n").unwrap();
+                self.screen.flush().unwrap();
+                true
+            },
+            Ok(any_key) => {
+                write!(self.screen, "I don't know what to do with {:?}\n", any_key).unwrap();
+                self.screen.flush().unwrap();
+                false
+            },
+            Err(e) => panic!("{:?}", e),
         }
     }
 }
 
+struct Buffer {
+}
+
+struct Cursor {
+}
+
 fn main() {
     println!("Hello, world!");
-    println!("{:?}", Editor::new().lines);
+    Editor::new().run();
 }
